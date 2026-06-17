a577cc2ff632fc198db55f3c50bca15b9508a69f
jackszb <55304530+jackszb@users.noreply.github.com>
2026-06-17 13:07:43 +0800

    Clean up URL list and fix main function call
    Removed unused URLs from the list and ensured proper function call.

--- merge_and_compile.py
+++ merge_and_compile.py
@@ -35,9 +35,7 @@ PROXY_URLS = [
   "https://raw.githubusercontent.com/jackszb/sukka-surge/main/non_ip/my_us.json",
   "https://raw.githubusercontent.com/jackszb/sukka-surge/main/non_ip/stream.json",
   "https://raw.githubusercontent.com/jackszb/sukka-surge/main/non_ip/telegram.json",
-  "https://raw.githubusercontent.com/jackszb/adguard-rules/main/json/filter_37_NoGoogle_filter.json",
   "https://raw.githubusercontent.com/jackszb/rules-set/main/rule-set/merged-domain-proxy.json",
-  "https://raw.githubusercontent.com/jackszb/rules-build/main/rules-src/porn.json",
 ]
 
 REJECT_URLS = [
